24def3dc7001827c4fe70038299bdee013da6dbe
seanpisano <sean@seanpisano.com>
2017-05-09 18:37:07 -0400

    so hmmm. not what i wanted yet.

--- final-project/script.js
+++ final-project/script.js
@@ -1,144 +1,85 @@
-//var map = new google.maps.Map(document.querySelector('#map'), {
-//	center: {
-// 		lat: 40.7128,
-// 		lng: -74.0059
-//  	},
-//  	zoom: 12,
-// });
+var map = new google.maps.Map(document.querySelector('#map'), {
+center: {
+lat: 40.7128,
+lng: -74.0059
+},
+zoom: 12,
+});
 
- //var marker = new google.maps.Marker({
- //	position: {
-//		lat: 40.7128,
-//		lng: -74.0059
-//	},
- //	map: map,
-// });
+var marker = new google.maps.Marker({
+position: {
+lat: 40.7128,
+lng: -74.0059
+},
+map: map,
+});
 
+//Venue section
 
 $.ajax({
-    type: 'GET',
-    url: 'https://api.untappd.com/v4/beer/info/3839',
-    success: function (response) {
-    console.log(response)
- //     var firstStation = response[0];
- //     console.log(response [0])
-//$('#container')
-//.append('<div>' + firstStation.doing_business_as_dba + '</div>')
-//.append('<div>Bikes Available: ' + firstStation.city + '</div>')
-//.append('<div>Spaces Free: ' + firstStation.premises_name + '</div>')
-  
+ype: 'GET',
+url: 'https://api.untappd.com/v4/beer/checkins/179?client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
+success: function (response) {
+//the loop  
+response.response.checkins.items.forEach (function (checkin) {
+//console.log(checkin);
+//
+var res = {venues: [],//}
 
-//create this loop  
-//response.forEach(function (station) {
+if (checkin.venue.venue_id) {
+$('#container')
+.append('<div>location: ' + checkin.venue.venue_name + '</div>')
+.append('<div>City: ' + checkin.venue.location.venue_city + '</div>')
+.append('<div>Address: ' + checkin.venue.location.venue_address + '</div>')
+.append('<div>Country: ' + checkin.venue.location.venue_country + '</div>')
+.append('<div>State: ' + checkin.venue.location.venue_state + '</div>')
+.append('<hr>');
 
-//$('#container')
-// .append('<div>' + station.doing_business_as_dba + '</div>')
-// .append('<div>City: ' + station.city + '</div>')
-// .append('<div>Place name: ' + station.premises_name+ '</div>')
-// .append('<hr>');
-
-//console.log(station);
-
- //var marker = new google.maps.Marker({
- // position: {
-//    lat: station.latitude,
-//    lng: station.longitude,
-//  },
-//  map: map,
-//      })
- //   })
-  },
+var marker = new google.maps.Marker({
+position: {
+lat: checkin.venue.location.lat,
+lng: checkin.venue.location.lng,
+},
+map: map,
+})
+ } else {  
+}
+})
+},
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Search
 //$.ajax({
- // 	type: 'GET',
- // 	url: 'http://api.citybik.es/citi-bike-nyc.json',
-//  	success: function (response) {
-  		//console.log(response)
-  		//var firstStation = response[0];
-  		//console.log(response [0])
-//$('#container')
-//.append('<div>' + firstStation.name + '</div>')
-//.append('<div>Bikes Available: ' + firstStation.bikes + '</div>')
-//.append('<div>Spaces Free: ' + firstStation.free + '</div>')
-	
-
-//create this loop	
-//response.forEach(function (station) {
-
-//$('#container')
-// .append('<div>' + station.name + '</div>')
-// .append('<div>Bikes Available: ' + station.bikes + '</div>')
-// .append('<div>Spaces Free: ' + station.free + '</div>')
-// .append('<hr>');
-
-//console.log(station);
-
-// var marker = new google.maps.Marker({
- //	position: {
-//		lat: station.lat / 1000000,
-//		lng: station.lng / 1000000,
-//	},
-// 	map: map,
-//			})
-// 		})
-//	},
+//    type: 'GET',
+//    url: 'https://api.untappd.com/v4/search/beer?q=Dogfish+60&client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
+//    success: function (search) {
+//  console.log(search);
+//the loop  
+//search.response.beers.items.forEach (function (bid) {
+////$('#container')
+//.append('<div>location: ' + bid + '</div>');
+//.append('<div>City: ' + checkin.venue.location.venue_city + '</div>')
+//.append('<div>Address: ' + checkin.venue.location.venue_address + '</div>')
+//.append('<div>Country: ' + checkin.venue.location.venue_country + '</div>')
+//.append('<div>State: ' + checkin.venue.location.venue_state + '</div>')
+//.append('<hr>');
+//   })
+//  },
 //});
 
 
+//Notes
+//var res = {
+//  venues: [],
+//}
 
+//if (!Array.isArray(res.venues)) {
+  // only runs if the `venues` key DOES NOT corresponds to an Array obj
+//}
 
+//```if (Array.isArray(res.venue) === false) {
+  // only runs if the `venue` key DOES NOT corresponds to an Array obj
+//}```
 
-
-//
-//$.ajax({
-//  	type: 'GET',
-//  	url: 'https://data.ny.gov/resource/83cw-i36h.json',
-//  	success: function (response) {
- // 		console.log(response)
- // 		var firstStation = response[0];
- // 		console.log(response [0])
-//$('#container')
-//.append('<div>' + firstStation.doing_business_as_dba + '</div>')
-//.append('<div>Bikes Available: ' + firstStation.city + '</div>')
-//.append('<div>Spaces Free: ' + firstStation.premises_name + '</div>')
-	
-
-//create this loop	
-//response.forEach(function (station) {
-
-//$('#container')
-// .append('<div>' + station.doing_business_as_dba + '</div>')
-// .append('<div>City: ' + station.city + '</div>')
-// .append('<div>Place name: ' + station.premises_name+ '</div>')
-// .append('<hr>');
-
-//
- //var marker = new google.maps.Marker({
- //	position: {
-//		lat: station.latitude,
-//		lng: station.longitude,
-//	},
-// 	map: map,
-//			})
-// 		})
-//	},
-//});
-
-
+//`if (venue.venue_id) { ....`
