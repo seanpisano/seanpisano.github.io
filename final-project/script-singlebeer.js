9cf10416d42f635699dda9fd250813b02bba01d6
seanpisano <sean@seanpisano.com>
2017-05-10 16:53:51 -0400

    almost where I want it to be I have to add design to it.

---
+++ final-project/script-singlebeer.js
@@ -0,0 +1,56 @@
+var map = new google.maps.Map(document.querySelector('#map'), {
+center: {
+lat: 40.7128,
+lng: -74.0059
+},
+zoom: 12,
+});
+
+var marker = new google.maps.Marker({
+position: {
+lat: 40.7128,
+lng: -74.0059
+},
+map: map,
+});
+
+//Venue section
+
+$.ajax({
+ype: 'GET',
+url: 'https://api.untappd.com/v4/beer/checkins/179?client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
+success: function (response) {
+//the loop  
+response.response.checkins.items.forEach (function (checkin) {
+console.log(checkin);
+
+var res = {venues: [],}
+
+if (checkin.venue.venue_id) {
+$('#container')
+.append('<div>location: ' + checkin.venue.venue_name + '</div>')
+.append('<div>City: ' + checkin.venue.location.venue_city + '</div>')
+.append('<div>Address: ' + checkin.venue.location.venue_address + '</div>')
+.append('<div>Country: ' + checkin.venue.location.venue_country + '</div>')
+.append('<div>State: ' + checkin.venue.location.venue_state + '</div>')
+.append('<hr>');
+
+var marker = new google.maps.Marker({
+position: {
+
+lat: checkin.venue.location.lat,
+lng: checkin.venue.location.lng,
+
+},
+
+map: map,
+})
+
+ } else {  
+}
+
+})
+},
+});
+
+
--- final-project/script.js
+++ final-project/script.js
@@ -1,85 +1,56 @@
-var map = new google.maps.Map(document.querySelector('#map'), {
-center: {
-lat: 40.7128,
-lng: -74.0059
-},
-zoom: 12,
-});
+//var map = new google.maps.Map(document.querySelector('#map'), {
+//center: {
+//lat: 40.7128,
+//lng: -74.0059
+//},
+//zoom: 12,
+//});
 
-var marker = new google.maps.Marker({
-position: {
-lat: 40.7128,
-lng: -74.0059
-},
-map: map,
-});
+//var marker = new google.maps.Marker({
+//position: {
+//lat: 40.7128,
+//lng: -74.0059
+//},
+//map: map,
+//});
 
 //Venue section
-
 $.ajax({
 ype: 'GET',
-url: 'https://api.untappd.com/v4/beer/checkins/179?client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
+url: 'https://api.untappd.com/v4/brewery/info/94785?client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
 success: function (response) {
+//  console.log(response);
 //the loop  
-response.response.checkins.items.forEach (function (checkin) {
-//console.log(checkin);
-//
-var res = {venues: [],//}
+response.response.brewery.checkins.items.forEach (function (checkin) {
+console.log(checkin);
+
+var res = {venues: [],}
 
 if (checkin.venue.venue_id) {
 $('#container')
-.append('<div>location: ' + checkin.venue.venue_name + '</div>')
-.append('<div>City: ' + checkin.venue.location.venue_city + '</div>')
-.append('<div>Address: ' + checkin.venue.location.venue_address + '</div>')
-.append('<div>Country: ' + checkin.venue.location.venue_country + '</div>')
-.append('<div>State: ' + checkin.venue.location.venue_state + '</div>')
-.append('<hr>');
-
-var marker = new google.maps.Marker({
-position: {
-lat: checkin.venue.location.lat,
-lng: checkin.venue.location.lng,
-},
-map: map,
-})
- } else {  
-}
-})
-},
-});
-
-
-//Search
-//$.ajax({
-//    type: 'GET',
-//    url: 'https://api.untappd.com/v4/search/beer?q=Dogfish+60&client_id=99506217035F59F9B0F16CE0841A88C4B27618F8&client_secret=667D5C8C35194D7CE4DBD863917D01A4207DF777',
-//    success: function (search) {
-//  console.log(search);
-//the loop  
-//search.response.beers.items.forEach (function (bid) {
-////$('#container')
-//.append('<div>location: ' + bid + '</div>');
-//.append('<div>City: ' + checkin.venue.location.venue_city + '</div>')
+.append('<div>Beer: ' + checkin.beer.beer_name + '</div>')
+.append('<div>Venue: ' + checkin.venue.venue_name + '</div>')
 //.append('<div>Address: ' + checkin.venue.location.venue_address + '</div>')
 //.append('<div>Country: ' + checkin.venue.location.venue_country + '</div>')
 //.append('<div>State: ' + checkin.venue.location.venue_state + '</div>')
 //.append('<hr>');
-//   })
-//  },
-//});
+
+//var marker = new google.maps.Marker({
+//position: {
+
+//lat: checkin.venue.location.lat,
+//lng: checkin.venue.location.lng,
+
+//},
+
+//map: map,
+//})
+
+ } else {  
+}
+
+})
+},
+});
 
 
-//Notes
-//var res = {
-//  venues: [],
-//}
-
-//if (!Array.isArray(res.venues)) {
-  // only runs if the `venues` key DOES NOT corresponds to an Array obj
-//}
-
-//```if (Array.isArray(res.venue) === false) {
-  // only runs if the `venue` key DOES NOT corresponds to an Array obj
-//}```
-
-//`if (venue.venue_id) { ....`
